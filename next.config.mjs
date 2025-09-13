a88516db70357b2ea091dab4187fb766a98295d0
praveen.b <praveen.b@xerago.com>
2025-09-13 21:08:43 +0530

    New commit

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/My-New-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/My-New-portfolio/' : '',
 }
 
 export default nextConfig
